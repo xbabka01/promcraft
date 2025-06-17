5330da927e54b87286bb96a7a7ddbbd6d342e1fe
Peter Babka <159peter951@gmail.com>
2025-06-18 01:46:03 +0200

    progress

--- tests/operators/test_binary.py
+++ tests/operators/test_binary.py
@@ -4,7 +4,7 @@ import prom_ql.operators as operators
 from prom_ql.literals import InstantVector
 from _pytest.fixtures import SubRequest
 
-INOP = [
+INFIX_OP = [
     # arithmetic operators
     "add",
     "sub",
@@ -25,9 +25,13 @@ INOP = [
     "complement",
 ]
 
+PREFIX_OP = [
+    "atan2",
+]
 
-@pytest.fixture(params=INOP)
-def operator(request: SubRequest) -> str:
+
+@pytest.fixture(params=INFIX_OP)
+def infix_operator(request: SubRequest) -> str:
     return request.param  # type: ignore[no-any-return]
 
 
@@ -36,13 +40,13 @@ def group(request: SubRequest) -> str | None:
     return request.param  # type: ignore[no-any-return]
 
 
-def test_binop(operator: str) -> None:
-    fn = getattr(operators, operator)
-    assert callable(fn), f"{operator} should be a callable function"
+def test_infix_op(infix_operator: str) -> None:
+    fn = getattr(operators, infix_operator)
+    assert callable(fn), f"{infix_operator} should be a callable function"
 
-    left = InstantVector(metric="test_metric", labels=[])
-    right = InstantVector(metric="test_metric", labels=[])
+    left = InstantVector(metric="left", labels=[])
+    right = InstantVector(metric="right", labels=[])
     result = fn(left=left, right=right)
     assert str(result) == f"({left}) {result.operator} ({right})", (
-        f"{operator} should return a valid expression"
+        f"{infix_operator} should return a valid expression"
     )
