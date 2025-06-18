cae1433cebe2040b87dd3a383a4dffe8806ae215
Peter Babka <159peter951@gmail.com>
2025-06-18 02:00:07 +0200

    progress

--- tests/operators/test_binary.py
+++ tests/operators/test_binary.py
@@ -4,6 +4,8 @@ import prom_ql.operators as operators
 from prom_ql.literals import InstantVector
 from _pytest.fixtures import SubRequest
 
+from prom_ql.operators.binary import LabelList
+
 INFIX_OP = [
     # arithmetic operators
     "add",
@@ -36,17 +38,34 @@ def infix_operator(request: SubRequest) -> str:
 
 
 @pytest.fixture(params=[None, "left", "right"])
-def group(request: SubRequest) -> str | None:
-    return request.param  # type: ignore[no-any-return]
+def group(request: SubRequest) -> tuple[str, LabelList] | None:
+    x = request.param
+    if x is None:
+        return None
+    return x, LabelList(["test"])
 
 
-def test_infix_op(infix_operator: str) -> None:
+@pytest.fixture(params=[None, "on", "ignoring"])
+def match(request: SubRequest) -> tuple[str, LabelList] | None:
+    x = request.param
+    if x is None:
+        return None
+    return x, LabelList(["test"])
+
+
+def test_base_infix_op(
+    infix_operator: str, group: tuple[str, LabelList] | None, match: tuple[str, LabelList]
+) -> None:
     fn = getattr(operators, infix_operator)
     assert callable(fn), f"{infix_operator} should be a callable function"
 
     left = InstantVector(metric="left", labels=[])
     right = InstantVector(metric="right", labels=[])
-    result = fn(left=left, right=right)
-    assert str(result) == f"({left}) {result.operator} ({right})", (
+    result = fn(left=left, right=right, group=group, match=match)
+
+    x = f" group_{group[0]}({group[1]})" if group is not None else ""
+    y = f" {match[0]}({match[1]})" if match is not None else ""
+
+    assert str(result) == f"({left}) {result.operator}{y}{x} ({right})", (
         f"{infix_operator} should return a valid expression"
     )
