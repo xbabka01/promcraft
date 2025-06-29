db9dee8ef7e25ceecbd6ea01f5f6154d5f7bc8a1
Peter Babka <159peter951@gmail.com>
2025-06-30 00:21:44 +0200

    add functions

---
+++ src/prom_ql/funtions.py
@@ -0,0 +1,15 @@
+
+from ast import Expression
+from dataclasses import dataclass
+from typing import ClassVar
+
+
+@dataclass(slots=True)
+class Function(Expression):
+    name: ClassVar[str]
+    params: list[Expression]
+    
+    def __str__(self) -> str:
+        params = ", ".join(str(param) for param in self.params)
+        return f"{self.name}({params})"
+    
